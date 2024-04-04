fe8599fcd0ef2719f45539d1eed075ebc17e2475
João Félix <felixjoao651@gmail.com>
2024-04-03 22:00:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '14': '3,5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'     
+       },
+       backgroundImage: theme => ({
+        'spotfy-theme': "url('/img/bursts.svg')",
+        'spotfy-theme-mobile': "url('/img/bursts-mobile.svg')",
+       }),
+       backgroundSize:{
+        '175%': '175%',
+        '195%': '195%',
+       },
+       backgroundPosition:{
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+       },
+       fontFamily:{
+        '9xl':'9rem'
+       }
+
+      },
+    },
+    variants:{},
+  plugins: [],
+}
